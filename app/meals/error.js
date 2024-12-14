788509491545c1716088f4337b3783365af4e900
nsigalmlly <116219159+nsigalmlly@users.noreply.github.com>
2024-12-14 13:47:59 -0300

    project

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+"use client"
+export default function Error () {
+    return <main className="error">
+        <h1>An error ocurred!</h1>
+        <p>Failed to fetch meals data, try again later</p>
+    </main>
+}
--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,6 +1,21 @@
 import classes from './page.module.css'
 import Link from 'next/link'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+export const metadata = {
+    title: 'All meals',
+    description: 'Browse the delicious meals shared by our vibrant community.',
+  };
+
+async function Meals() {
+    const meals = await getMeals();
+        return <MealsGrid meals={meals}></MealsGrid>
+}
+
 export default function MealsPage(){
+
     return (<>
     <header className={classes.header}>
         <h1>Delicious meals, created <span className={classes.highlight}>by you</span> </h1>
@@ -9,6 +24,10 @@ export default function MealsPage(){
             <Link href='/meals/share'>Share your favorite recipe</Link>
         </p>
     </header>
-    <main className={classes.main} ></main>
+    <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+            <Meals />
+        </Suspense>
+    </main>
     </>)
 }
